254c3e7587796735a01a1ec8e14169979eb12563
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-12 00:20:47 +0530

    Solved Revising_the_Select_Query_II

---
+++ 01-Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------
+Problem    : Revising the Select Query II
+
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 12-July-2026
+-------------------------------------------------------
+*/
+
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCode = 'USA'
