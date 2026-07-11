aa8e732ee0edcff6fde37a3e6d534739d4b702aa
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-12 00:16:02 +0530

    Solved Revising_the_Select_Query_I

---
+++ 01-Basic Select/Revising_the_Select_Query_I.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Revising_the_Select_Query_I
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 12-July-2026
+-------------------------------------------------------
+*/
+
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCode = 'USA'
