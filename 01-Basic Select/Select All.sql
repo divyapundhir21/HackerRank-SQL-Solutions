9d031f18c9b98520cc12ef10a06b733711f5c941
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-12 00:29:46 +0530

    Select_All

---
+++ 01-Basic Select/Select All.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Select All
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/select-all-sql/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 12-July-2026
+-------------------------------------------------------
+*/
+
+SELECT * FROM CITY
