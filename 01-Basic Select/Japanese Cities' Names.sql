a79a47d8d94974c9bb1f37cea9208445ad492a01
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-13 23:08:31 +0530

    Japanese Cities' Names

---
+++ 01-Basic Select/Japanese Cities' Names.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Japanese Cities' Names
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 13-July-2026
+-------------------------------------------------------
+*/
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN'
