5c98b7b3d9a84ac070556464cd8892d97470e9f5
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-13 23:04:05 +0530

    Solved Select By ID

---
+++ 01-Basic Select/Select By ID.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Select By ID
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/select-by-id/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 13-July-2026
+-------------------------------------------------------
+*/
+
+SELECT * FROM CITY WHERE ID = 1661
