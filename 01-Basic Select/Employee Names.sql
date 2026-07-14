addd3a48bd0c22d44ed1da9968684057c50be6ad
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-14 09:25:16 +0530

    Employee Names

---
+++ 01-Basic Select/Employee Names.sql
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------
+Problem    : Employee Names
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/name-of-employees/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 14-July-2026
+-------------------------------------------------------
+*/
+
+
+SELECT name from EMPLOYEE ORDER BY NAME ASC
