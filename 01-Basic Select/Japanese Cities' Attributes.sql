91946ef9b0d8938cf9341eed8d86927e1a48bd52
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-15 23:05:52 +0530

    Weather Observation Station 1

---
+++ 01-Basic Select/Japanese Cities' Attributes.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Japanese Cities' Attributes
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 15-July-2026
+-------------------------------------------------------
+*/
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN'
---
+++ 01-Basic Select/Weather Observation Station 1.sql
@@ -0,0 +1,20 @@
+/*
+-------------------------------------------------------
+Problem    : Weather Observation Station 1
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 15-July-2026
+-------------------------------------------------------
+*/
+
+
+SELECT CITY, STATE FROM STATION 
