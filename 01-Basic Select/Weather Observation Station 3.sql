f4b3ff2c48813f1ffd221c2c45a382e6c6258e53
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-16 09:05:12 +0530

    Weather Observation Station 3

---
+++ 01-Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------
+Problem    : Weather Observation Station 3
+Platform   : HackerRank
+Domain     : Basic Select
+Difficulty : Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+Concepts Used:
+- SELECT
+
+Author     : Divya Pundhir
+Date       : 16-July-2026
+-------------------------------------------------------
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE (ID % 2 = 0)
